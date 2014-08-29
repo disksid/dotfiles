e6559490b2c425d34b83c48c0007dd39e2a3ebd8
disksid <disksid@gmail.com>
2014-08-29 12:00:44 +0900

    140829

--- set_link.cmd
+++ set_link.cmd
@@ -1,7 +1,8 @@
 SET DOTFILES_PATH=%~dp0
 CD %DOTFILES_PATH%
-del %USERPROFILE%\.vimrc
-mklink %USERPROFILE%\.vimrc %DOTFILES_PATH%.vimrc
-del %USERPROFILE%\.gvimrc
-mklink %USERPROFILE%\.gvimrc %DOTFILES_PATH%.gvimrc
-pause
+
+FOR %%F IN (.*) DO (
+  DEL %USERPROFILE%\%%F
+  MKLINK %USERPROFILE%\%%F %DOTFILES_PATH%%%F
+)
+PAUSE
